ab8b0e7bc7d50202b5dabeb760b3b4478da26675
Niraj <nkgangale@gmail.com>
2022-03-19 14:51:19 +0530

    Add FindCharacter.java file

---
+++ 004 - Conditionals and Loops/02 - Assingments+Extra/codes/src/FindCharacter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindCharacter {
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scan = new Scanner(System.in);
+        char ch = scan.next().charAt(0);
+        if (ch >= 'A'&& ch <='Z'){
+            System.out.println(1);
+        }
+        else if (ch >='a'&& ch <= 'z'){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(-1);
+        }
+
+    }
+}
