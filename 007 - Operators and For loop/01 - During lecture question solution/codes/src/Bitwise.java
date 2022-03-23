ce6e12d23da7c38e25b1c6a78ee1227811ac1c59
Niraj <nkgangale@gmail.com>
2022-03-23 11:29:00 +0530

    Add Bitwise files.

---
+++ 007 - Operators and For loop/01 - During lecture question solution/codes/src/Bitwise.java
@@ -0,0 +1,14 @@
+
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println(2&3);
+        System.out.println(2|3);
+        System.out.println(2^3);
+        System.out.println(~2);
+        System.out.println(-7>>1);//-4
+        System.out.println(-7<<1);//-14
+        System.out.println(7<<1);// 14
+        System.out.println(7>>1);// 3
+
+    }
+}
