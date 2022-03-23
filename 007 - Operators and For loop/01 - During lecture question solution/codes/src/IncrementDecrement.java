2d6e41f0ebcb13cbe0e032e1b94f4626e3ba1044
Niraj <nkgangale@gmail.com>
2022-03-23 15:32:04 +0530

    Add incrementDecrement files.

---
+++ 007 - Operators and For loop/01 - During lecture question solution/codes/src/IncrementDecrement.java
@@ -0,0 +1,18 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println(a);
+        // post increment
+        System.out.println(a++);
+        a = 5;
+        // post decrement
+        System.out.println(a--);
+        a = 5;
+        // pre increment
+        System.out.println(++a);
+        a = 5;
+        // pre decrement
+        System.out.println(--a);
+    }
+}
