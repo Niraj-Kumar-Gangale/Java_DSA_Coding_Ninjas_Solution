25582b43ec7c109fec121a3d31ab02dc32c5a8b2
Niraj <nkgangale@gmail.com>
2022-06-06 15:40:58 +0530

    Update Nth Prime Number.

--- 007 - Operators and For loop/02 - Assingments+Extra/codes/src/NthFibonacciNumber.java
+++ 007 - Operators and For loop/02 - Assingments+Extra/codes/src/NthFibonacciNumber.java
@@ -1,30 +1,32 @@
-/*      Nth Fibonacci Number
+/***
+ * Nth Fibonacci Number
+ * Send Feedback
+ * Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+ *     F(n) = F(n-1) + F(n-2),
+ *     Where, F(1) = F(2) = 1
+ * Provided N you have to find out the Nth Fibonacci Number.
+ * Input Format :
+ * The first line of each test case contains a real number ‘N’.
+ * Output Format :
+ * For each test case, return its equivalent Fibonacci number.
+ * Constraints:
+ * 1 <= N <= 10000
+ * Where ‘N’ represents the number for which we have to find its equivalent Fibonacci number.
+ *
+ * Time Limit: 1 second
+ * Sample Input 1:
+ * 6
+ * Sample Output 1:
+ * 8
+ * Explanation of Sample Input 1:
+ * Now the number is ‘6’ so we have to find the “6th” Fibonacci number
+ * So by using the property of the Fibonacci series i.e
+ *
+ * [ 1, 1, 2, 3, 5, 8]
+ * So the “6th” element is “8” hence we get the output
+ */
 
-Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
-    F(n) = F(n-1) + F(n-2),
-    Where, F(1) = F(2) = 1
-Provided N you have to find out the Nth Fibonacci Number.
-Input Format :
-The first line of each test case contains a real number ‘N’.
-Output Format :
-For each test case, return its equivalent Fibonacci number.
 
-Constraints:
-1 <= N <= 10000
-Where ‘N’ represents the number for which we have to find its equivalent Fibonacci number.
-
-Time Limit: 1 second
-
-Sample Input 1:
-6
-Sample Output 1:
-8
-Explanation of Sample Input 1:
-Now the number is ‘6’ so we have to find the “6th” Fibonacci number
-So by using the property of the Fibonacci series i.e
-
-[ 1, 1, 2, 3, 5, 8]
-So the “6th” element is “8” hence we get the output.*/
 
 import java.util.Scanner;
 public class NthFibonacciNumber {
@@ -32,7 +34,7 @@ public class NthFibonacciNumber {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int a = 0, b=1;
-        int c = 0;
+        int c ;
         for (int i =1; i<=n;i++){
             c= a+b;
             a=b;
