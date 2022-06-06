0ebe14b9a54031edc53ef4e05cc723d43db20e79
Niraj <nkgangale@gmail.com>
2022-06-06 17:01:17 +0530

    Update reverse of a number.

---
+++ 007 - Operators and For loop/02 - Assingments+Extra/codes/src/ReverseOfANumber.java
@@ -0,0 +1,40 @@
+/**
+ * Reverse of a number
+ * Send Feedback
+ * Write a program to generate the reverse of a given number N. Print the corresponding reverse number.
+ * Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+ *
+ *
+ * Input format :
+ * Integer N
+ * Output format :
+ * Corresponding reverse number
+ * Constraints:
+ * 0 <= N < 10^8
+ * Sample Input 1 :
+ * 1234
+ * Sample Output 1 :
+ * 4321
+ * Sample Input 2 :
+ * 1980
+ * Sample Output 2 :
+ * 891
+ */
+
+import java.util.Scanner;
+public class ReverseOfANumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        int newNumber = 0;
+        int temp = number;
+        // don't use the original scanned value for calculation
+        while(temp>0){
+            int remainder = temp%10;
+            newNumber = 10*newNumber + remainder;
+            temp = temp/10;
+        }
+        System.out.println(newNumber);
+    }
+
+}
