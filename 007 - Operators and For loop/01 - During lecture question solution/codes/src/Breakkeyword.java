2f1683a82c7e53d09effa3f811793c374ca334dd
Niraj <nkgangale@gmail.com>
2022-06-06 12:47:12 +0530

    Add BreakKeyword file.

--- 007 - Operators and For loop/01 - During lecture question solution/codes/src/Breakkeyword.java
+++ 007 - Operators and For loop/01 - During lecture question solution/codes/src/Breakkeyword.java
@@ -1,2 +1,43 @@
+
+import java.util.Scanner;
 public class Breakkeyword {
+
+    // create a java function for the prime number question
+    // to implement the use of the break key word
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        // using the boolean function and break
+        boolean isPrime = true;
+        int divisor = 2;
+        while (divisor<number){
+            if(number%divisor==0){
+                isPrime= false;
+                break;
+            }
+            divisor++;
+        }
+        if(isPrime){
+            System.out.println(" Prime Number ");
+        }else{
+            System.out.println(" Not A Prime number");
+        }
+
+        // using return
+
+        divisor = 2;
+        while (divisor<number){
+            if(number%divisor==0){
+                System.out.println(" Not A Prime number");
+                return;
+            }
+            divisor++;
+        }
+
+        System.out.println(" Prime Number ");
+
+        // you can also make function for the prime number [the easiest solution]
+
+    }
 }
