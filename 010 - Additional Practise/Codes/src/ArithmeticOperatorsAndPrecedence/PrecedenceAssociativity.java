045e6256ba580d3744aa67bc8adf986a755b7d86
Niraj <nkgangale@gmail.com>
2022-06-08 16:39:52 +0530

    Add precedence Associativity Files.

---
+++ 010 - Additional Practise/Codes/src/ArithmeticOperatorsAndPrecedence/PrecedenceAssociativity.java
@@ -0,0 +1,14 @@
+package ArithmeticOperatorsAndPrecedence;
+
+public class PrecedenceAssociativity {
+    public static void main(String[] args) {
+
+        // associativity
+        int c = 3+10/5;
+        System.out.println(c);
+
+        // (/ * %) have same precedence, so we do it using associativity.
+        // associativity for (/ * %) is from left to right
+        int d = 10/2*3;
+    }
+}
