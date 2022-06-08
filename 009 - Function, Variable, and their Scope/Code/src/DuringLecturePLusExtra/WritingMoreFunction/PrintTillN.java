79c4a2941ae4d100be225f7a4f86953c6f6eadff
Niraj <nkgangale@gmail.com>
2022-06-08 09:16:32 +0530

    Add Print 1 to N file.

---
+++ 009 - Function, Variable, and their Scope/Code/src/DuringLecturePLusExtra/WritingMoreFunction/PrintTillN.java
@@ -0,0 +1,14 @@
+package DuringLecturePLusExtra.WritingMoreFunction;
+
+public class PrintTillN {
+
+    public static void PrintTillNNumber (int n){
+        for(int i = 1 ; i<=n ; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintTillNNumber(10);
+    }
+}
