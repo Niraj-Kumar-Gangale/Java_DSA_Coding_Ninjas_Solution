734d8ce17566836e4b0c0e71951f884b71ce8d8e
Niraj <nkgangale@gmail.com>
2022-06-08 16:40:23 +0530

    Add Arithmetic Operators Files.

---
+++ 010 - Additional Practise/Codes/src/ArithmeticOperatorsAndPrecedence/ArithmeticOperators.java
@@ -0,0 +1,37 @@
+package ArithmeticOperatorsAndPrecedence;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        /*
+        we will talk about the arithmetic operator such as-
+        addition +
+        subtraction -
+        multiplication *
+        division /
+        modulo i.e remainder %
+         */
+
+        // addition
+        int add = 45+12;
+        System.out.println(add);
+
+        // subtraction
+        int sub = 45-4;
+        System.out.println(sub);
+
+        // multiplication
+        int mul = 15*6;
+        System.out.println(mul);
+
+        // division
+        int div = 45/2;
+        System.out.println(div);
+        // answer comes out will be whole number because ((int/int)=int)
+
+        // modulo
+        int md = 45%2;
+        System.out.println(md);
+
+
+    }
+}
