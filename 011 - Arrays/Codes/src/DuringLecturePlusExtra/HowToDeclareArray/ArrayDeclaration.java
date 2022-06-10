1308fc4dccc5e680b53fdd6d2b6074da93a5f582
Niraj <nkgangale@gmail.com>
2022-06-10 12:33:28 +0530

    Add Array Declaration Files.

---
+++ 011 - Arrays/Codes/src/DuringLecturePlusExtra/HowToDeclareArray/ArrayDeclaration.java
@@ -0,0 +1,20 @@
+package DuringLecturePlusExtra.HowToDeclareArray;
+
+public class ArrayDeclaration {
+    public static void main(String[] args) {
+        //  We will see how  to declare the array of fixed size
+        // two ways to declare
+        // FIRST
+
+        int[] firstArray ; // only declaration No Space is allocated.
+        firstArray = new int[10]; // Here Space Is Allocated.
+
+        // SECOND
+        // Declaration and Initialization both in single line.
+        int[] secondArray = new int[20];
+
+        char[] thirdArray;
+        thirdArray = new char[56];
+    }
+
+}
