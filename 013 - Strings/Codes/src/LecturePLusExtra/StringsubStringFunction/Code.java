16364d1fc81313a9ed3bb0fb7a4d5d649e60eade
Niraj <nkgangale@gmail.com>
2022-06-25 17:13:05 +0530

    Add use of subString function file.

---
+++ 013 - Strings/Codes/src/LecturePLusExtra/StringsubStringFunction/Code.java
@@ -0,0 +1,13 @@
+package LecturePLusExtra.StringsubStringFunction;
+
+public class Code {
+
+    public static void main (String[] args){
+
+        // use of the subString function in the String Non-Primitive data type
+        String  str1 = "Noble Niraj";
+        System.out.println(str1.substring(5));
+        System.out.println(str1.substring(0 ,4));
+    }
+
+}
