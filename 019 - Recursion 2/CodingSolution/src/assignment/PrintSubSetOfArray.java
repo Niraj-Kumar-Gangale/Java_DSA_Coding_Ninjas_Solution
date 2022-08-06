c454c81b2389deaae5e0f095627c6f6de8342068
Niraj <nkgangale@gmail.com>
2022-08-06 10:38:10 +0530

    Update files.

--- 019 - Recursion 2/CodingSolution/src/assignment/PrintSubSetOfArray.java
+++ 019 - Recursion 2/CodingSolution/src/assignment/PrintSubSetOfArray.java
@@ -63,4 +63,24 @@ public class PrintSubSetOfArray {
         outputArray = temp;
         printSubsets(input, si+1, outputArray);
     }
+    // coding ninja
+    public static void printSubsetsHelper(int input[], int beginIndex, int[] output){
+        if(beginIndex == input.length){
+            for(int i : output){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+        int[] newOutput = new int[output.length+1];
+        int i = 0;
+        for(; i < output.length; i++){
+            newOutput[i] = output[i];
+        }
+        newOutput[i] = input[beginIndex];
+
+        printSubsetsHelper(input, beginIndex+1,output);
+        printSubsetsHelper(input, beginIndex+1, newOutput);
+    }
+
 }
