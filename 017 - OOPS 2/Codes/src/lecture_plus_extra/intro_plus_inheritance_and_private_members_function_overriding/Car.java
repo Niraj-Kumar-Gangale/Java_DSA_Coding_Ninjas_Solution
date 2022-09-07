b5f2be1a7b2860cb5b61b12bda56347d2c6bf9b3
Niraj <nkgangale@gmail.com>
2022-09-07 16:34:40 +0530

    Update files.

--- 017 - OOPS 2/Codes/src/lecture_plus_extra/intro_plus_inheritance_and_private_members_function_overriding/Car.java
+++ 017 - OOPS 2/Codes/src/lecture_plus_extra/intro_plus_inheritance_and_private_members_function_overriding/Car.java
@@ -4,6 +4,53 @@ public class Car extends Vehicle{
     int gearNo;
     boolean isConvertible;
 
+    String name;
+
+    int maxSpeed;
+
+    public Car(int maxSpeed){
+        super(maxSpeed);
+    }
+
+
+
+    public void printMaxSpeed(){
+        System.out.println(super.getMaxSpeed());
+    }
+
+    @Override
+    public void setMaxSpeed(int maxSpeed) {
+        System.out.println("Car is Called");
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        System.out.println("Car is Called");
+        return maxSpeed;
+    }
+
+
+    public int getSuperMaxSpeed() {
+        return super.getMaxSpeed();
+    }
+
+
+    public void setSuperMaxSpeed(int maxSpeed) {
+        super.setMaxSpeed(maxSpeed);
+    }
+
+    // let's define name again to see the working of super keyword
+//    String name;
+//
+//    // print name function to check for the working of the super keyword
+//        public boolean printName(){
+//            super.name = "dumboo";
+//        System.out.println("super.name "+ super.name);
+//        System.out.println("name "+ name);
+//        return false;
+//    }
+
     public void print(){
         System.out.println("Name "+ name);
         // if maxSpeed is public then below written is possible
@@ -16,6 +63,29 @@ public class Car extends Vehicle{
 
     }
 
+
+    public void printName() {
+
+        System.out.println(super.name);
+    }
+
+    public void setSuperName(String name){
+        super.name = name;
+    }
+//------------------------------------------------------------------------------------------------
+    // well answer for the both will come as same because we did not  define
+    // an extra name here if we did then
+    // name will tell the name for the car
+    // super.name will tell not tell the name for the vehicle
+    // because there i no relation between the objects of the
+    // VEHICLE AND CAR class
+
+//    public boolean printName(){
+//        System.out.println("super.name "+ super.name);
+//        System.out.println("name "+ name);
+//        return false;
+//    }
+//------------------------------------------------------------------------------------------------
     public void fun(int number){
         System.out.println(" fun(int number) ");
     }
