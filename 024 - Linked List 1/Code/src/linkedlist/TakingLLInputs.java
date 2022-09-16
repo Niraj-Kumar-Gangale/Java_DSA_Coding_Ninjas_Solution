a9e1aef038a67531597a006a607421aedf9fd9fc
Niraj <nkgangale@gmail.com>
2022-09-16 09:50:47 +0530

    Add TakingLLInput code.

---
+++ 024 - Linked List 1/Code/src/linkedlist/TakingLLInputs.java
@@ -0,0 +1,72 @@
+package linkedlist;
+
+
+import java.util.Scanner;
+
+public class TakingLLInputs {
+
+    /**
+     * <li>Method to take Input for the linkedList.</li>
+     * Taking input until -1 is provided. If input -1 then the return the Head reference value.
+     * <p>Works for integer only</p>
+     * Time Complexity O(n^2).
+     */
+    public static Node<Integer> takingInput (){
+
+        Node<Integer> head = null;
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+
+        while (data !=-1){
+            Node<Integer> newNode = new Node<>(data);
+            if (head ==null){
+                head = newNode;
+            }else{
+                Node<Integer> temp = head ;
+                while (temp.next != null){
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+
+    /**
+     * <li>Method to take Input for the linkedList.</li>
+     * Taking input until -1 is provided. If input -1 then the return the Head reference value.
+     * <p>Works for integer only</p>
+     * Time Complexity O(n).
+     */
+    public static Node<Integer> takingInput2 (){
+
+        Node<Integer> head = null, tail = null;
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+
+        while (data !=-1){
+            Node<Integer> newNode = new Node<>(data);
+            if (head ==null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head ;
+        head = takingInput();
+        LinkedListPrint.printLinkedList(head);
+        Node<Integer> head2 ;
+        head2 = takingInput();
+        LinkedListPrint.printLinkedList(head2);
+    }
+
+}
