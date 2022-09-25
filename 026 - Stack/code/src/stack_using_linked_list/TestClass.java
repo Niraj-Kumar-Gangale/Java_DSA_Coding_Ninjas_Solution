df152565cc3fe3b84fd0571730f2ef2e7a3d70e0
Niraj <nkgangale@gmail.com>
2022-09-25 15:35:42 +0530

    Add test case.

---
+++ 026 - Stack/code/src/stack_using_linked_list/TestClass.java
@@ -0,0 +1,14 @@
+package stack_using_linked_list;
+
+public class TestClass {
+
+//    public static Object[] getArray(int size) {
+//        Object[] genericArray = new Object[size];
+//        return genericArray;
+//    }
+
+    public static void main(String[] args) {
+        String[] myArray = new String[45];
+        Object[] myObject = myArray;
+    }
+}
